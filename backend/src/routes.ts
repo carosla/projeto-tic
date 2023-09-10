0a3c726aab30791a7378d9d232c35c9999741d87
Caio <caiotaveiradecastro@gmail.com>
2023-09-10 16:58:37 -0300

    Apagando number

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,5 @@
 import {FastifyInstance} from 'fastify'
-import {number, z} from 'zod'
+import {z} from 'zod'
 import {prisma} from './lib/prisma'
 
 export async function AppRoutes(server:FastifyInstance){
